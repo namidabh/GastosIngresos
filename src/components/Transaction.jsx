b98a14edd026041f237797dc63c24346eff9df56
namidabh <bphb26@gmail.com>
2021-01-26 21:34:44 -0400

    nodes

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types'
     Componente que lee datos de un arreglo de objetos, el cual esta en el contexto global,
     que representa cada transaccion, y luego renderiza tantas
     transacciones hayan
+
 */}
 
 export default function Transaction(props) {
